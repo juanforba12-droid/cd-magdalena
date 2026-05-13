2cb0c48749096764b065b46362c573e0784992a0
juanforba12-droid <juanforba12@gmail.com>
2026-05-13 10:11:23 +0000

    safelist player color classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  safelist: [
+    'bg-red-600','border-red-400',
+    'bg-yellow-500','border-yellow-300',
+    'bg-blue-600','border-blue-400',
+    'bg-green-600','border-green-400',
+  ],
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
